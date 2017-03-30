ff13736787f4564262f448e44435714748ac5bd4
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2017-03-30 12:19:40 +0200

    Tests umbennenung

--- AD_Praktikum/src/testsJunit/ListenTests.java
+++ AD_Praktikum/src/testsJunit/ListenTests.java
@@ -13,38 +13,38 @@ import aufg1_listen.*;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class FunctionTests
+public class ListenTests
 {	
 	
 	@SuppressWarnings("unused")
-	public void testInsert(IList<String> liste)
+	public void testInsert(IList<String> liste , String key)
 	{
 		liste.insert("TestElement 1", liste.find(""));
 		liste.insert("TestElement 2", liste.find(""));
 		liste.insert("TestElement 3", liste.find(""));
 		liste.insert("TestElement 4", liste.find(""));
-		assertEquals("TestElement 3", liste.retrieve(liste.find("Element 4")));
+		assertEquals("TestElement 3", liste.retrieve(liste.find(key)));
 	}
 	
-	@Test
-	public void testInsertListA()
-	{
-		IList<String> liste = new ListeA();
-		testInsert(liste);
-	}
+//	@Test
+//	public void testInsertListA()
+//	{
+//		IList<String> liste = new ListeA();
+//		testInsert(liste, "3");
+//	}
 	
 	@Test
 	public void testInsertListB()
 	{
 		IList<String> liste = new ListeB();
-		testInsert(liste);
+		testInsert(liste,"Element 4");
 	}
 	
 	@Test
 	public void testInsertListC()
 	{
 		IList<String> liste = new ListeC();
-		testInsert(liste);
+		testInsert(liste,"Element 4");
 	}
 
 }
