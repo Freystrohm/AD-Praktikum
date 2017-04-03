7f5fdc5920260e1511052d46ad7c49bc505add42
Johannes <Johannes@Jojo-Desktop>
2017-04-03 21:07:20 +0200

    Listen Tests Delete

--- AD_Praktikum/src/testsJunit/ListenTests.java
+++ AD_Praktikum/src/testsJunit/ListenTests.java
@@ -7,16 +7,14 @@ package testsJunit;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import aufg1_listen.*;
-import junit.framework.Assert;
-import junit.framework.TestCase;
+
 
 public class ListenTests
 {	
 	
-	@SuppressWarnings("unused")
+	
 	public void testInsert(IList<String> liste , String key)
 	{
 		liste.insert("TestElement 1", liste.find(""));
@@ -36,15 +34,30 @@ public class ListenTests
 	@Test
 	public void testInsertListB()
 	{
-		IList<String> liste = new ListeB();
+		IList<String> liste = new ListeB<String>();
 		testInsert(liste,"Element 4");
 	}
 	
 	@Test
 	public void testInsertListC()
 	{
-		IList<String> liste = new ListeC();
+		IList<String> liste = new ListeC<String>();
 		testInsert(liste,"Element 4");
 	}
+	
+	@Test
+	public void testDeleteB()
+	{
+		IList<String> liste = new ListeB<String>();
+		liste.insert("TestElement 1", liste.find(""));
+		liste.insert("TestElement 2", liste.find(""));
+		liste.insert("TestElement 3", liste.find(""));
+		liste.insert("TestElement 4", liste.find(""));
+		
+		liste.delete("Element 3");
+		assertEquals(null, liste.retrieve(liste.find("Element 3")));		
+	}
+	
+	
 
 }
