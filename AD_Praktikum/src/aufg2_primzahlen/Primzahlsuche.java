2c16876242cf8909d0a74a2399505d0db5496ee3
Johannes <Johannes@Jojo-Desktop>
2017-04-03 22:44:05 +0200

    anpassungen der Primzahlsuche

--- AD_Praktikum/src/aufg2_primzahlen/Primzahlsuche.java
+++ AD_Praktikum/src/aufg2_primzahlen/Primzahlsuche.java
@@ -1,38 +1,70 @@
 package aufg2_primzahlen;
 
-public class Primzahlsuche {
+import java.util.Arrays;
 
-	
-	public Primzahlsuche(){
+public class Primzahlsuche
+{
+	public static int zaehlerLangsam = 0;
+	public static int zahlerSchnell = 0;
+
+	public Primzahlsuche()
+	{
 	}
-	
-	public static boolean[] langsameSuche(int n){
-		
-		boolean[] a=new boolean[n];
-		for ( int i = 0; i < n ; i++)
-		a[i] = true;
-		for (int  i = 2; i < n; i++){
-			for ( int j = 2; j < n; j++){
-				if ( (i%j == 0) && (j!=i) ){
+
+	public static boolean[] langsameSuche(int n)
+	{
+
+		boolean[] a = new boolean[n];
+		for (int i = 0; i < n; i++)
+			a[i] = true;
+		for (int i = 2; i < n; i++)
+		{
+			for (int j = 2; j < n; j++)
+			{
+				zaehlerLangsam++;
+				if ((i % j == 0) && (j != i))
+				{
 					a[i] = false;
-				}		
+				}
 			}
 		}
 		return a;// alle i fuer die a[i] noch auf true steht, sind Primzahlen
 	}
-	public static boolean[] schnelleSuche(int n){
-		
-		boolean[] a=new boolean[n];
-		for ( int i = 0; i < n ; i++)
-		a[i] = true;
-		for (int  i = 2; i < n; i++){
-			for ( int j = 2; j < Math.sqrt(i); j++){
-				if ( (i%j == 0) && (j!=i) ){
+
+	// Schneller durch: suche bis wurzel i, insert eines breaks und entfernen
+	// von (j != i) abfrage
+	public static boolean[] schnelleSuche(int n)
+	{
+
+		boolean[] a = new boolean[n];
+		Arrays.fill(a, Boolean.TRUE);
+		for (int i = 0; i < n; i++)
+			a[i] = true;
+		for (int i = 2; i < n; i++)
+		{
+			for (int j = 2; j < Math.sqrt(i); j++)
+			{
+				zahlerSchnell++;
+				if ((i % j == 0))
+				{
 					a[i] = false;
-				}		
+					break;
+				}
 			}
 		}
 		return a;// alle i fuer die a[i] noch auf true steht, sind Primzahlen
+	}
+
+	public boolean istPrimzahl(int n)
+	{
+		for (int j = 2; j < Math.sqrt(n); j++)
+		{
+			if ((n % j == 0))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
