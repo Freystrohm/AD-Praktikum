94e4883f29022e5702c43dda51ea38d44a02a255
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2017-04-24 12:20:50 +0200

    Init QuickSort

---
+++ AD_Praktikum/src/aufg5_QuickSort/QuickSort.java
@@ -0,0 +1,17 @@
+/**
+ * PM2 Praktikum
+ * @author Johannes Kruber
+ * @author Luis Nickel 
+ */
+package aufg5_QuickSort;
+
+public class QuickSort
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
