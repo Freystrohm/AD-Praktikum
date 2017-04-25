9e32af1b164ea9ba6fc16fa247c20a171a03376f
Johannes <Johannes@Jojo-Desktop>
2017-04-25 19:27:46 +0200

    Quicksort manually tested

--- AD_Praktikum/src/aufg5_QuickSort/QuickSort.java
+++ AD_Praktikum/src/aufg5_QuickSort/QuickSort.java
@@ -1,7 +1,5 @@
 /**
- * PM2 Praktikum
  * @author Johannes Kruber
- * @author Luis Nickel 
  */
 package aufg5_QuickSort;
 
@@ -14,10 +12,10 @@ public class QuickSort<T extends Comparable<T>>
 		this.array = array;
 	}
 
-	public T findPivotMedian()
+	private T findPivotMedian(int links, int rechts)
 	{
-		T fEle = array[0], lEle = array[array.length - 1],
-				mEle = array[array.length / 2];
+		T fEle = array[links], lEle = array[rechts],
+				mEle = array[(links + rechts) / 2];
 		if ((fEle.compareTo(lEle) < 0 && fEle.compareTo(mEle) > 0)
 				|| (fEle.compareTo(lEle) > 0 && fEle.compareTo(mEle) < 0))
 		{
@@ -33,20 +31,28 @@ public class QuickSort<T extends Comparable<T>>
 		{
 			return lEle;
 		}
+		else
+		{
+			return mEle;
+		}
 
-		return null;
+		// return null;
 	}
 
-	public T findPivotRdm()
+	private T findPivotRdm(int links, int rechts)
 	{
-		return array[(int) (Math.random() * (array.length - 1))];
+		return array[((int) (Math.random() * (rechts - links))) + links];
 	}
 
-	@SuppressWarnings("unchecked")
-	private T[] sortToLast(T ele, T[] array) throws Exception
+	private T findPivotLast(int rechts)
+	{
+		return array[rechts];
+	}
+
+	private void sortToLast(T ele, int links, int rechts) throws Exception
 	{
 		int index = -1;
-		for (int i = 0; i < array.length; i++)
+		for (int i = links; i <= rechts; i++)
 		{
 			if (ele.compareTo(array[i]) == 0)
 			{
@@ -59,30 +65,99 @@ public class QuickSort<T extends Comparable<T>>
 		{
 			throw new Exception("Element nicht vorhanden");
 		}
-
-		Object[] temp = new Object[array.length];
-		System.arraycopy(array, 0, temp, 0, index);
-		System.arraycopy(array, index + 1, temp, index,
-				array.length - index - 1);
-		temp[temp.length - 1] = ele;
-		return (T[]) temp;
-	}
-
-	public T findPivotLast()
-	{
-		return array[array.length - 1];
-	}
-
-	public void quicksort(int links, int rechts, T pivot)
-	{
-		int ilinks = 0, irechts = array.length - 2;
+		swap(index, rechts);
 
 	}
 
-	public static void main(String[] args)
+	private T findPivot(int links, int rechts, int suche) throws Exception
 	{
-		// TODO Auto-generated method stub
+		T pivot;
+		switch (suche)
+		{
+			case 1 :
+				pivot = findPivotLast(rechts);
+				break;
+			case 2 :
+				pivot = findPivotMedian(links, rechts);
+				break;
+			case 3 :
+				pivot = findPivotRdm(links, rechts);
+				break;
+			default :
+				pivot = array[links];
+		}
 
+		sortToLast(pivot, links, rechts);
+		return pivot;
+	}
+
+	public void quicksort(int links, int rechts, int suche) throws Exception
+	{
+		if (links >= rechts)
+		{
+			return;
+		}
+		T pivot = findPivot(links, rechts, suche);
+		int ilinks = links, irechts = rechts - 1;
+
+		while (true)
+		{
+			while (array[ilinks].compareTo(pivot) < 0)
+			{
+				ilinks++;
+			}
+			while (array[irechts].compareTo(pivot) > 0 && irechts != ilinks)
+			{
+				irechts--;
+			}
+
+			if (ilinks == irechts)
+			{
+				break;
+			}
+			else if (ilinks > irechts)
+			{
+				irechts = ilinks;
+				System.out.println("ilinks > irechts");
+				break;
+			}
+
+			swap(ilinks, irechts);
+		}
+		swap(irechts, rechts);
+		quicksort(links, ilinks - 1, suche);
+		quicksort(irechts + 1, rechts, suche);
+	}
+
+	private void swap(int ilinks, int irechts)
+	{
+		T temp = array[ilinks];
+		array[ilinks] = array[irechts];
+		array[irechts] = temp;
+	}
+
+	public void shuffle()
+	{
+		for (int i = 0; i < array.length * 100; i++)
+		{
+			swap((int) (Math.random() * (array.length)),
+					(int) (Math.random() * (array.length)));
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Integer[] array = new Integer[30];
+		for (int i = 1; i <= array.length; i++)
+		{
+			array[i - 1] = i;
+		}
+		QuickSort<Integer> sort = new QuickSort<Integer>(array);
+
+		sort.shuffle();
+
+		sort.quicksort(0, array.length - 1, 3);
+		System.out.println("Ende");
 	}
 
 }
