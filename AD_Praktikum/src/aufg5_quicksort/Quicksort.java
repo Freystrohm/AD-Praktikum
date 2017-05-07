eb03dc6fe5ca562714b2da3b04c2f1ff3d7d4c02
Luis Nickel <luis.nickel@hotmail.de>
2017-05-07 09:58:42 +0200

    x

--- AD_Praktikum/src/aufg5_quicksort/Quicksort.java
+++ AD_Praktikum/src/aufg5_quicksort/Quicksort.java
@@ -9,10 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+
 
 
 public class Quicksort<T extends Comparable<T>> {
